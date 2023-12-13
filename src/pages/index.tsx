f680346cc9aec4f47c9a5ff2ed5f310c1f390345
Evgeniy Popov <evgeniy.popov@amatechs.com>
2023-12-13 10:56:50 +0600

    Add new feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        FIX
+        FEATURE
         <div className={styles.center}>
           <Image
             className={styles.logo}
