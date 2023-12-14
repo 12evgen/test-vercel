8eb5eecab00b3f28d39e92014373feadb99ccbb3
Evgeniy Popov <evgeniy.popov@amatechs.com>
2023-12-14 11:53:41 +0600

    feat: New

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        New Package
+        New Package new
         <div className={styles.center}>
           <Image
             className={styles.logo}
