983f9dad48f62771db08eb26f562450e9231843f
Evgeniy Popov <evgeniy.popov@amatechs.com>
2023-12-14 11:00:24 +0600

    feat: New release test 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        New
+        New fdsfds fdsf
         <div className={styles.center}>
           <Image
             className={styles.logo}
