a07eb54fb16f28d26bcbd757f1cbffb22f7d8b79
Evgeniy Popov <evgeniy.popov@amatechs.com>
2023-12-14 12:17:48 +0600

    feat: new github package

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        New Package
+        New Github Package
         <div className={styles.center}>
           <Image
             className={styles.logo}
