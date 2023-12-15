be1ae90a2c9a435b78f7b1464aab433858a721ad
Evgeniy Popov <evgeniy.popov@amatechs.com>
2023-12-15 12:53:21 +0600

    Pre Production 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        Pre Production
+        Pre Production Production Production Production
         <div className={styles.center}>
           <Image
             className={styles.logo}
