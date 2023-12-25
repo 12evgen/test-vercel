07b8b43b453a65e3368d0b1f3b0181e57c426dcd
Evgeniy Popov <evgeniy.popov@amatechs.com>
2023-12-25 13:03:36 +0600

    fix: test pr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        New version v1.49.0 (test)
+        New version v1.49.0 (PR)
         <div className={styles.center}>
           <Image
             className={styles.logo}
