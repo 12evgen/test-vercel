6fda8a3c7968f380e1cb8983c97fe4205f01b67b
Evgeniy Popov <evgeniy.popov@amatechs.com>
2023-12-28 15:38:22 +0600

    fix: change code 49

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        New version v1.49.0 (1.52.25)
+        New version v1.49.0 (1.52.26)
         <div className={styles.center}>
           <Image
             className={styles.logo}
