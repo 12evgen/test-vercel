0883b5f8649500395c98cb3704967318472c52ef
Evgeniy Popov <evgeniy.popov@amatechs.com>
2023-12-29 12:22:22 +0600

    perf: perf test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        New version refactor
+        New version perf
         <div className={styles.center}>
           <Image
             className={styles.logo}
