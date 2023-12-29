ea24eb68773e326810e712c23edb9645175e2a84
Evgeniy Popov <evgeniy.popov@amatechs.com>
2023-12-29 12:16:33 +0600

    refactor: refactor test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        New version v1.53.0
+        New version refactor
         <div className={styles.center}>
           <Image
             className={styles.logo}
