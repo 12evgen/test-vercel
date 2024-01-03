9617aea16ba7fc2b192d18c20d3214e225191cf1
Evgeniy Popov <evgeniy.popov@amatechs.com>
2024-01-03 12:01:44 +0600

    chore!: test major version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        New version perf
+        New version MAJOR
         <div className={styles.center}>
           <Image
             className={styles.logo}
